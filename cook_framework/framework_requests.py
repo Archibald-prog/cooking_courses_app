d5b6efccf557c75afb99d38fa834c6feb6e906f9
[Archibald] <[archpod@gmail.com]>
2025-04-02 13:14:53 +0300

    added PostRequests class

--- cook_framework/framework_requests.py
+++ cook_framework/framework_requests.py
@@ -24,3 +24,49 @@ class GetRequests:
         # Преобразуем строку в словарь
         request_params = GetRequests.parse_input_data(query_string)
         return request_params
+
+
+# Обработка POST-запросов
+class PostRequests:
+
+    @staticmethod
+    def parse_input_data(data: str) -> dict:
+        result = {}
+        if data:
+            params = data.split('&')
+            for item in params:
+                k, v = item.split('=')
+                result[k] = v
+        return result
+
+    @staticmethod
+    def get_wsgi_input_data(env) -> bytes:
+        """
+        Метод возвращает тело запроса в байтах
+        :param env: словарь environ
+        :return:
+        """
+        content_length_data = env.get('CONTENT_LENGTH')
+        if content_length_data:
+            content_length = int(content_length_data)
+        else:
+            content_length = 0
+        if content_length > 0:
+            data = env['wsgi.input'].read(content_length)
+        else:
+            data = b''
+        return data
+
+    def parse_wsgi_input_data(self, data: bytes) -> dict:
+        result = {}
+        if data:
+            # Декодируем данные запроса в строковый формат
+            data_str = data.decode(encoding='utf-8')
+            # Преобразуем данные из строки в словарь
+            result = self.parse_input_data(data_str)
+        return result
+
+    def get_request_params(self, environ):
+        data = self.get_wsgi_input_data(environ)
+        data = self.parse_wsgi_input_data(data)
+        return data
