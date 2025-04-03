bce704eea6c6a13f900978fb76676d60615f9f73
[Archibald] <[archpod@gmail.com]>
2025-04-03 16:19:41 +0300

    added jinja2.Environment object to work with templates

--- cook_framework/templator.py
+++ cook_framework/templator.py
@@ -1,14 +1,9 @@
-from os.path import join
-from jinja2 import Template
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
-    file_path = join(folder, template_name)
-    with open(file_path, encoding='utf-8') as f:
-        # получаем шаблон в формате строки
-        # и передаем его в конструктор класса Template
-        template = Template(f.read())
-    # вызываем метод render класса Template,
-    # передавая ему словарь с контекстом -
-    # получаем рез-т рендеринга в строковом формате
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
